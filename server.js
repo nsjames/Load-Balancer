01dac6c5bf3820dc16bdbafc9aae089636ee8d99
nsjames <skyandbeyond@gmail.com>
2019-01-29 22:35:40 +0200

    test

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const handler = () => (req, res) => {
 		// req.pipe(x).pipe(res);
 		res.json(JSON.parse(x));
 	}).catch(err => {
-		console.error(err);
+		res.json(JSON.parse(err));
 		serverDown(server);
 
 		// Recurse until server found
